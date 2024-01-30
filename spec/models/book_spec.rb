11d82e7d67589397ad0fc48c053aa0e6a6b3202c
Abdulhameed A <69105027+ahameed02@users.noreply.github.com>
2024-01-30 05:36:36 +0000

    completed BC2

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  it 'is valid with a valid title' do
-    book = Book.new(title: 'Valid Title')
-    expect(book).to be_valid
-  end
+    it 'is valid with a valid title, author, price, and published date' do
+      book = Book.new(title: 'Valid Title', author: 'Author Name', price: 19, published_date: Date.today)
+      expect(book).to be_valid
+    end
 
   it 'is invalid without a title' do
     book = Book.new(title: nil)
--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Books", type: :request do
-  describe "POST /books" do
-    context "with valid parameters" do
-      it "creates a new Book and redirects to the Book's page with a notice" do
-        expect {
-          post books_path, params: { book: { title: 'New Book' } }
-        }.to change(Book, :count).by(1)
-
-        follow_redirect!
-        expect(response.body).to include('Book was successfully created.')
-        expect(response.body).to include('New Book')
+    describe "POST /books" do
+        context "with valid parameters" do
+          it "creates a new Book and redirects to the Book's page with a notice" do
+            expect {
+              post books_path, params: { book: { title: 'New Book', author: 'Author Name', price: 9.99, published_date: Date.today } }
+            }.to change(Book, :count).by(1)
+    
+            expect(response).to redirect_to(Book.last)
+            follow_redirect!
+            expect(response.body).to include('Book was successfully created.')
       end
     end
 
